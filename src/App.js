a3af903804fbdd1ecd2cfa672a24c72449e43a82
FervKa <90405746+FervKa@users.noreply.github.com>
2021-11-06 16:32:27 -0500

    Se empieza a modificar el código

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './images/neptuno.png';
+import logo from './images/logo-neptuno-bordeado.png';
 import './App.css';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Nepuno, Aplicación de gestión de proyectos
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Proximamente
-        </a>
-      </header>
+      
+
     </div>
   );
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,23 @@
+import logo from './images/logo-neptuno-bordeado.png';
+import './App.css';
+
+import React from 'react'
+
+export const navbar = () => {
+  return (
+    <div className="App">
+      <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src={logo} alt="" width="30" height="24" class="d-inline-block align-text-top"></img>
+            Neptuno
+          </a>
+        </div>
+      </nav>
+
+    </div>
+  )
+}
+
+
+
