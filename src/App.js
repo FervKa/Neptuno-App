1b15eb22a6f2776ff4aad930300afcd80cd740ba
William Tavera (wstavera) <ws.tavera@gmail.com>
2021-11-06 11:43:35 -0500

    probando inicio

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './images/neptuno.png';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Nepuno, Aplicación de gestión de proyectos
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Proximamente
         </a>
       </header>
     </div>
