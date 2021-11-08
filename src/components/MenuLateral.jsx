0b02ca3ba3d1c7cd624270f2983f848012726110
FervKa <90405746+FervKa@users.noreply.github.com>
2021-11-08 16:35:26 -0500

    Se crean apartados y se les da animación

--- src/components/MenuLateral.jsx
+++ src/components/MenuLateral.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import menuLateral from '../css/menuLateral.css'
 import logo from '../images/favicon.png'
+import user from '../images/user_sinfondo.png'
 
 export const MenuLateral = () => {
     return (
         <>
             <div className="menu_lateral">
-                <div className="sidebar">
+                <div className="sidebar close">
                     <div className="logo-details">
-                        <img src={logo}  alt="icono-logo" className ="imagen-ico" />
-                        <spam className="logo_name">Neptuno</spam>
+                        <img src={logo} alt="icono-logo" className="imagen-ico" />
+                        <span className="logo_name">Neptuno</span>
                     </div>
                     <ul className="nav-links">
                         <li>
@@ -17,6 +18,9 @@ export const MenuLateral = () => {
                                 <i className='bx bx-grid-alt' ></i>
                                 <span className="link_name">Dashboard</span>
                             </a>
+                            <ul className="sub-menu blank">
+                                <li><a className="link_name" href="#">Categoria</a></li>
+                            </ul>
                         </li>
                         <li>
                             <div className="icon-links">
@@ -26,17 +30,94 @@ export const MenuLateral = () => {
                                 </a>
                                 <i className='bx bxs-chevron-down arrow'></i>
                             </div>
+                            <ul className="sub-menu">
+                                <li><a className="link_name" href="#">Categoria</a></li>
+                                <li><a href="#">PRUEBA 1</a></li>
+                                <li><a href="#">PRUEBA 2</a></li>
+                                <li><a href="#">PRUEBA 3</a></li>
+                            </ul>
                         </li>
                         <li>
                             <div className="icon-links">
                                 <a href="#">
-                                    <i className='bx bx-collection' ></i>
+                                    <i className='bx bx-book-bookmark'></i>
                                     <span className="link_name">Posteos</span>
                                 </a>
                                 <i className='bx bxs-chevron-down arrow'></i>
                             </div>
+                            <ul className="sub-menu">
+                                <li><a className="link_name" href="#">Categoria</a></li>
+                                <li><a href="#">PRUEBA 1</a></li>
+                                <li><a href="#">PRUEBA 2</a></li>
+                                <li><a href="#">PRUEBA 3</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className='bx bx-pie-chart-alt-2' ></i>
+                                <span className="link_name">Analítica</span>
+                            </a>
+                            <ul className="sub-menu blank">
+                                <li><a className="link_name" href="#">Analítica</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className='bx bx-line-chart-down'></i>
+                                <span className="link_name">Gráficos</span>
+                            </a>
+                            <ul className="sub-menu blank">
+                                <li><a className="link_name" href="#">Gráficos</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <div className="icon-links">
+                                <a href="#">
+                                    <i className='bx bx-plug' ></i>
+                                    <span className="link_name">Comple</span>
+                                </a>
+                                <i className='bx bxs-chevron-down arrow'></i>
+                            </div>
+                            <ul className="sub-menu">
+                                <li><a className="link_name" href="#">Categoria</a></li>
+                                <li><a href="#">PRUEBA 1</a></li>
+                                <li><a href="#">PRUEBA 2</a></li>
+                                <li><a href="#">PRUEBA 3</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className='bx bx-compass' ></i>
+                                <span className="link_name">Explora</span>
+                            </a>
+                            <ul className="sub-menu blank">
+                                <li><a className="link_name" href="#">Explora</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className='bx bx-history' ></i>
+                                <span className="link_name">Historia</span>
+                            </a>
+                            <ul className="sub-menu blank">
+                                <li><a className="link_name" href="#">Historia</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i className='bx bx-cog' ></i>
+                                <span className="link_name">Configuración</span>
+                            </a>
+                            <ul className="sub-menu blank">
+                                <li><a className="link_name" href="#">Configuración</a></li>
+                            </ul>
                         </li>
                     </ul>
+                    <div className="profile-details">
+                        <div className="profile-content">
+                            <img src={user} alt="user" className="user-logo" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
