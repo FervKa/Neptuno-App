a913c32760a8efab39f184e66ba165ebc33566fa
FervKa <90405746+FervKa@users.noreply.github.com>
2021-11-08 17:25:36 -0500

    Se solucionó error de componentes en minúscula

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import {MenuLateral} from "./components/MenuLateral.jsx"
-import {Navbar} from "./components/Navbar.jsx"
+
 
 
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
       <MenuLateral />
-
     </>
   );
 }
