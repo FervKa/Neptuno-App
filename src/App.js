9cb3520a8c36fbcfd94990fc7d63f3d1ffff24ed
William Tavera (wstavera) <ws.tavera@gmail.com>
2021-11-11 19:47:30 -0500

    Perfil de usuario front Ok

--- src/App.js
+++ src/App.js
@@ -6,13 +6,8 @@ import { User } from './components/User.jsx';
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      {/* <MenuLateral /> */}
-      <User />
-=======
       <MenuLateral />
-      {/* <User /> */}
->>>>>>> d26ffe6b9dcfdbb86533a6f64709c4ed2211dcea
+      <User />
     </>
   );
 }
