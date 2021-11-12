75f68410be9ac19ae47fa2140871820d98a79d44
Vicente Velasco <velasco.josevicente@gmail.com>
2021-11-11 20:26:37 -0500

    Se afinan detalles frontend login. Verificar el <a> de registrarse, opcional <link>

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,12 +9,18 @@ export const Login = () => {
                 <div align="center">
                     <img className="img-fluid" src={logoNeptunoBordeado} alt="Logo Neptuno" width="100px" />
                 </div>
-                <p className="logotipo-neptuno">NEPTUNO</p>
+                <p className="logotipo-naranja mb-3 fs-1">NEPTUNO</p>
+                <p className="text-wrap texto-naranja">Sistema de Gestión de Proyectos de Investigación</p>
                 <form action="POST" className="display-flex" >
-                    <input placeholder="Correo electronico" className="form-control" type="text" />
-                    <input placeholder="Contraseña" className="form-control" type="text" />
-                    <button className="botonNaranja btn">Ingresar</button>
+                    <input placeholder="Correo electronico" className="w-75 form-control mb-3" type="text" />
+                    <input placeholder="Contraseña" className="w-75 form-control mb-3" type="password" />
+                    <button className="botonNaranja btn mb-3 w-75">Ingresar</button>
                 </form>
+                <div className="row w-75 mx-auto">
+                    <p className="col texto-naranja">¿No tienes cuenta?</p>
+                    <a className="col" href="#"> Regístrate</a>
+
+                </div>
 
             </div>
 
