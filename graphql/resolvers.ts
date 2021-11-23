56130d291c2737d7449ccd6f37def89f70dd8a67
Vicente Velasco <velasco.josevicente@gmail.com>
2021-11-22 22:21:06 -0500

    Se empieza la mutacion para crear usuario

--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,13 +1,30 @@
 import { UserModel } from "../models/users"
 
-const resolvers ={
+const resolvers = {
 
-    Query:{
-        Usuarios : async (parent, args) => {
+    Query: {
+        Usuarios: async (parent, args) => {
             const usuarios = await UserModel.find()
-            return usuarios            
+            return usuarios
+        },
+    },
+
+    Mutation: {
+        crearUsuario: async (paren, args) => {
+            const usuarioCreado = await UserModel.create({
+                nombre: args.nombre,
+                apellido: args.apellido,
+                identificacion: args.identificacion,
+                correo: args.correo,                
+                rol: args.rol,
+            })
+
+            if (Object.keys(args).includes("estado")){//Esta validacion es necesaria pues de lo contrario solo tomaria el dato por defecto
+                usuarioCreado.estado=args.estado;
+            }
+            return usuarioCreado
         }
     }
 }
 
-export {resolvers}
+export { resolvers }
--- graphql/types.ts
+++ graphql/types.ts
@@ -1,8 +1,8 @@
-import {gql} from 'apollo-server-express';
+import { gql } from 'apollo-server-express';
 
 const typeDefs = gql`
     
-    enum Enum_RolUsario{
+    enum Enum_RolUsario{# GraphQL recomienda que los enumeradores se escriban en mayusculas. Estos tambien se cambiaron en los enumeradores de mongoose (archivo enums.ts)
         ESTUDIANTE
         LIDER
         ADMINISTRADOR
@@ -14,20 +14,31 @@ const typeDefs = gql`
         NO_AUTORIZADO
     }
     
-    type Usuario{
-        _id: ID!
+    type Usuario{ #Se definen los esquemas gql para cada documento nuevamente.
+        _id: ID! #El signo de exclamacion (!) indica que el campo es obligatorio.
         nombre:String!
         apellido: String!
         correo: String!
         identificacion:String!        
-        estado:Enum_EstadoUsuario!
-        rol: Enum_RolUsario!
+        estado:Enum_EstadoUsuario
+        rol: Enum_RolUsario
 
     }
 
-    type Query{
-        Usuarios:[Usuario]
+    type Query{#Se define una consulta, es decir, un READ
+        Usuarios:[Usuario] #La sintaxis es <resolver>:<Dato deseado>. El resolver trae los datos segun como se especifican. En este caso crea un array con datos tipo Usuario (los campos deseados se especifican en Apollo) definido mas arriba
+    }
+
+    type Mutation{
+        crearUsuario(
+            nombre:String!
+            apellido: String!
+            correo: String!
+            identificacion:String!        
+            #Los campos estado, rol e _id tienen valores por defecto. No son obligatorios
+            #El campo _id no es obligatorio al crear porque lo asigna mongoDB.
+        ):Usuario
     }
 `;
 
-export {typeDefs}
+export { typeDefs }
