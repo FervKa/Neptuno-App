86a2c84e2ec39f91bb0adfaccd63bda3668589c6
Cristian Gonzalo Sarmiento Castro <sarmientocastrocris@gmail.com>
2021-11-24 11:50:29 -0500

    se crea carpeta de graphql

---
+++ graphql/resolvers.ts
@@ -0,0 +1,12 @@
+const resolvers = {
+    Query: {
+        Usuarios: async (parent, args) =>{
+            const usuarios = [
+
+            ];
+            return usuarios;
+        },
+    },
+};
+
+export {resolvers};
