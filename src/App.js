30d8e475ed369914bcbccf7f78e62ae874509b35
William Tavera (wstavera) <ws.tavera@gmail.com>
2021-12-03 18:07:15 -0500

    formulario de Registro

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import {MenuLateral} from "./components/MenuLateral.jsx"
 import { User } from './components/User.jsx';
 import '../src/css/index.css';
 import { Login } from './components/Login';
+import { Registro } from "./components/Registro.jsx";
 
 
 function App() {
   return (
     <>
-      {/* <MenuLateral />
-      <User /> */}
-      <Login />
+      {/* <MenuLateral /> */}
+      {/* <User /> */}
+      {/* <Login /> */}
+      <Registro />
     </>
   );
 }
