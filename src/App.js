9fbb7212ef317434d9b2e81fa1239709b8266f19
Vicente Velasco <velasco.josevicente@gmail.com>
2021-12-06 23:50:59 -0500

    Se finaliza configuracion del apollo client

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import { User } from './components/User.jsx';
 import '../src/css/index.css';
 import { Login } from './components/Login';
 import { Registro } from "./components/Registro.jsx";
+import{ApolloClient, ApolloProvider, createHttpLink, InMemoryCache} from '@apollo/client'
 
+const httpLink = createHttpLink({
+  uri: 'https://neptuno-app.herokuapp.com/graphql',
+})
+
+
+const client = new ApolloClient({
+  uri: '',
+  cache: new InMemoryCache()
+});
 
 function App() {
+
+
   return (
-    <>
+    <ApolloProvider>
+
       {/* <MenuLateral /> */}
       {/* <User /> */}
       {<Login />}
       {/* <Registro /> */}
-    </>
+
+    </ApolloProvider>
+    
   );
 }
 
