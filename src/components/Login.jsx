c5dbf42777a6bbd952f8f738c1904d21753b4225
Vicente Velasco <velasco.josevicente@gmail.com>
2021-12-11 14:33:07 -0500

    Se inicia ruta de Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import "../css/estilos.scss";
 import logoNeptunoBordeado from '../images/logo-neptuno-bordeado.png';
 import { User } from './User';
 import { useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
-
+import useFormData from "../hooks/useFormData";
+import { useMutation } from '@apollo/client'
+import { LOGIN } from './graphql/auth/mutations'
 
 export const Login = () => {
+    const { form, formData, updateFormData } = useFormData(null)
 
     const navigate = useNavigate();
 
-    const handleUser = () => {
-        navigate('/perfil')
+    const [Ingreso, {data: mutationData, loading: mutationLoading, error: mutationError}]=useMutation(LOGIN)
+   
+
+    const submitForm =(e)=>{
+        e.preventDefault();
+        console.log("Formulario login", formData);
+        Ingreso({
+            variables:{formData}
+        })
     }
 
-    const submitForm =()=>{
-        console.log("Carga del ingreso");
-    }
+    useEffect(() => {
+        console.log('mutacion de ingreso', mutationData);
+    }, [mutationData])
 
 
     return <>
@@ -26,10 +36,10 @@ export const Login = () => {
             </div>
             <p className="logotipo-naranja mb-3 fs-1">NEPTUNO</p>
             <p className="text-wrap texto-naranja">Sistema de Gestión de Proyectos de Investigación</p>
-            <form onSubmit={submitForm} action="POST" className="display-flex" >
-                <input required placeholder="Correo electronico" className="w-75 form-control mb-3" type="text" />
-                <input required placeholder="Contraseña" className="w-75 form-control mb-3" type="password" />
-                <button type ='submit' onClick={handleUser} className="botonNaranja btn mb-3 w-75">Ingresar</button>
+            <form ref={form} onSubmit={submitForm} onChange={updateFormData} action="POST" className="display-flex" >
+                <input required name='correo' placeholder="Correo electronico" className="w-75 form-control mb-3" type="text" />
+                <input required name= 'password' placeholder="Contraseña" className="w-75 form-control mb-3" type="password" />
+                <button type ='submit' className="botonNaranja btn mb-3 w-75">Ingresar</button>
             </form>
             <div className="row w-75 mx-auto">
                 <p className="texto-naranja">¿No tienes cuenta? <Link to="/auth/registro"> Regístrate</Link></p>
