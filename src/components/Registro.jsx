85ea62821ae670dd85581da3ef91fdf5f017e062
Vicente Velasco <velasco.josevicente@gmail.com>
2021-12-11 01:11:07 -0500

    Se configura el FormData. Se toman los pares clave, valor del formulario de registro

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -9,13 +9,15 @@ export const Registro = () => {
     const submitForm = (e) => {
         e.preventDefault();
         const infoFormulario = new FormData(form.current)
-        infoFormulario.forEach((val, el) => {
-            console.log(val, el);
+        const usuarioNuevo = {}
+        infoFormulario.forEach((value, key) => {
+            usuarioNuevo[key] = value
         })
+        console.log(usuarioNuevo);
+
         console.log("Valores del formulario", infoFormulario);
         console.log("Valores del formulario", form.current);
     }
-
 
     return (
         <>
@@ -29,64 +31,66 @@ export const Registro = () => {
                     <p className="text-wrap texto-naranja">Sistema de Gestión de Proyectos de Investigación</p>
                 </div>
 
-                <div className="container" align='center'>
-                    <div className="col border-3">
-                        <form ref={form} onSubmit={submitForm}>
-                            <input
-                                required
-                                placeholder="Nombre"
-                                type="text"
-                                className="form-control isI  w-75 mb-3"
-                                id="nombres"
-                            />
-                            <input
-                                required
-                                placeholder="Apellido"
-                                type="text"
-                                className="form-control isI w-75  mb-3"
-                                id="apellidos"
-                            />
-                            <input
-                                required
-                                placeholder="Identificación"
-                                type="number"
-                                min={0}
-                                className="form-control isI w-75  mb-3"
-                                id="identificacion"
-                            />
-                            <select
-                                id="rol"
-                                className="form-select w-75 mb-3"
-                                required
-                            >
-                                <option disabled selected value="">
-                                    Rol
-                                </option>
-                                <option value="ESTUDIANTE">Estudiante</option>
-                                <option value="LIDER">Lider</option>
-                                <option value="ADMINISTRADOR">Administrador</option>
-                            </select>
-                            <input required
-                                placeholder="Correo electrónico"
-                                type="email"
-                                className="mb-3 w-75 form-control"
-                                id="correo" />
-                            <input
-                                required
-                                placeholder="Contraseña"
-                                type="password"
-                                className="form-control w-75 mb-3"
-                                id="password"
-                            />
-                            <button type='submit' className="col botonNaranja btn w-75 isI mb-3">
-                                Registrar
-                            </button>
-                        </form>
-                        <p className="texto-naranja" >¿Ya tienes cuenta? <Link to="/" > Ingresa</Link></p>
-                    </div>
-                </div>
+                <form className='display-flex' ref={form} onSubmit={submitForm}>
+                    <input
+                        required
+                        placeholder="Nombre"
+                        type="text"
+                        className="form-control w-75 mb-3"
+                        id="nombres"
+                        name="nombres"
+                    />
+                    <input
+                        required
+                        placeholder="Apellido"
+                        type="text"
+                        className="form-control w-75  mb-3"
+                        id="apellidos"
+                        name="apellidos"
+                    />
+                    <input
+                        required
+                        placeholder="Identificación"
+                        type="number"
+                        min={0}
+                        className="form-control w-75  mb-3"
+                        id="identificacion"
+                        name="identificacion"
+                    />
+                    <select
+                        id="rol"
+                        name="rol"
+                        className="form-select w-75 mb-3"
+                        required
+                    >
+                        <option disabled selected value="">
+                            Rol
+                        </option>
+                        <option value="ESTUDIANTE">Estudiante</option>
+                        <option value="LIDER">Lider</option>
+                        <option value="ADMINISTRADOR">Administrador</option>
+                    </select>
+                    <input required
+                        placeholder="Correo electrónico"
+                        type="email"
+                        className="mb-3 w-75 form-control"
+                        id="correo"
+                        name="correo"
+                    />
+                    <input
+                        required
+                        placeholder="Contraseña"
+                        type="password"
+                        className="form-control w-75 mb-3"
+                        id="password"
+                        name="password"
+                    />
+                    <button type='submit' className="col botonNaranja btn w-75 mb-3">
+                        Registrar
+                    </button>
+                </form>
+                <p className="texto-naranja" >¿Ya tienes cuenta? <Link to="/" > Ingresa</Link></p>
             </div>
-
         </>
     );
 };
