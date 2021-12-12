86fb1d9f86eea1d0ec26fea1fea76bd4bc0edfc2
William Tavera (wstavera) <ws.tavera@gmail.com>
2021-12-11 22:29:54 -0500

    se continua fornt de proyectos

--- src/components/Proyecto.jsx
+++ src/components/Proyecto.jsx
@@ -6,6 +6,7 @@ import { useQuery } from '@apollo/client';
 import { GET_PROYECTO } from './graphql/proyectos/querys';
 import { Navbar } from './Navbar';
 import { MenuLateral } from './MenuLateral';
+import { useState } from 'react';
 
 export const Proyecto = () => {
     const { _id  } = useParams()
@@ -13,13 +14,17 @@ export const Proyecto = () => {
     const { data, error, loading } = useQuery(GET_PROYECTO, {variables: { _id },
     });
 
-    /* const cargarProyecto = () => {
-        console.log(data)
-    } */
+    //const [objetivosProyecto, setObjetivosProyecto] = useState([])
+    let objetivosProyecto = []
+    
+    const cargarProyecto = () => { 
+        loading ? console.log("prueba"): objetivosProyecto= data.leerProyecto.objetivos
+        console.log(objetivosProyecto);
+    }
 
-    /* useEffect(() => {
+    useEffect(() => {
         cargarProyecto()
-    }, [data]) */
+    }, [data])
 
   return (
     <>    
@@ -48,6 +53,51 @@ export const Proyecto = () => {
                                 </div>
                             </div>
                         </div>}
+                    </div>
+
+                    <br />
+
+                    <div className="row">
+
+                        <div className="col">
+                        
+                            {
+                                objetivosProyecto.map((o) => (
+                                <div className="col" key={o._id}>
+                                    <div className="card">
+                                    <div className="card-body">
+
+                                    <h4 className="card-title">
+                                        tipo: {o.tipo}
+                                        </h4>
+                                        {/* <h6>Inicio: {p.fechaInicio}</h6>
+                                        <p className="card-text">Lider: {p.lider.nombres} {p.lider.apellidos}</p>
+                                        <p className="card-text">Estado: {p.estado}</p> */}
+                                    </div>
+                                    <div className="card-footer">
+                                       {/*  <small className="text-muted">${p.presupuesto}</small> */}
+                                    </div>
+                                    </div>
+                                </div>
+                                )) }
+                        </div>
+
+                        <div className="col">
+                        <div className="card">
+                                <div className="card-body">
+
+                                    <h4 className="card-title">
+                                        Avances
+                                        </h4>
+                                        <h6>tipo: {data.leerProyecto.avances.fechaAvance}</h6>
+                                       {/*  <p className="card-text">Descripcion: {data.leerProyecto.avance.descripcion} </p> */}
+                                        
+                                </div>
+                                <div className="card-footer">
+                                    {/* <small className="text-muted">{data.leerProyecto.avance._id}</small> */}
+                                </div>
+                            </div>
+                        </div>
                     </div>
 
                     <Link to="/proyectos" className="btn btn-primary" >regresar</Link>
